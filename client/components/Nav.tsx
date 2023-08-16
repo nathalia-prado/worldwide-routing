c4c575e29b277a291795447c56f7936caa632a53
Nathalia Prado <nathalia_898@hotmail.com>
2023-08-16 12:55:35 +1200

    add list of continents to the nav component

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,12 +1,15 @@
+import data from "../../data/continents"
+
 function Nav() {
   return (
     <div>
       <h2>Nav</h2>
       <ul>
-        <li>Build your continent list here</li>
+        {Object.keys(data).map(key => <li key={key}>{key} </li>)}
       </ul>
     </div>
   )
 }
 
 export default Nav
+
